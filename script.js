946fb8e835b99feaf6484fda4fc519cce4056a86
Carole Gérard <gerard.carole78@gmail.com>
2023-08-31 15:29:44 +0200

    Recreate a layout using a grid in CSS

---
+++ script.js
@@ -0,0 +1,12 @@
+// const buttons = document.querySelectorAll('article button');
+// const divs = document.querySelectorAll('article .line');
+
+// buttons.forEach((button, index) => {
+//     button.addEventListener('mouseover', () => {
+//         divs[index].style.opacity = '0';
+//     });
+
+//     button.addEventListener('mouseout', () => {
+//         divs[index].style.opacity = '1';
+//     });
+// });
